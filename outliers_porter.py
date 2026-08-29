b730128b53f0398c9655889f4a97fd384bbf3497
Trishita Saha <65353949+trishitasaha@users.noreply.github.com>
2026-08-29 23:44:36 +0530

    Update outliers_porter.py

--- outliers_porter.py
+++ outliers_porter.py
@@ -1,108 +1,12 @@
-#!/usr/bin/env python3
-# 
-# Copyright (c) 2002 Vivake Gupta (vivake@omniscia.org).  All rights reserved.
-# Python 3 modernization (c) 2026 - mechanical port of Vivake Gupta's original;
-# algorithm and structure unchanged. See git history for the full lineage.
-# 
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-# USA
-#
-# This software is maintained by Vivake (vivakeATomniscia.org) and is available at:
-#     https://omniscia.org/python/porter-stemmer
-
-
-"""Porter Stemming Algorithm
-
-This is the Porter stemming algorithm, ported to Python from the
-version coded up in ANSI C by the author. It follows the algorithm
-presented in
-
-Porter, M. "An algorithm for suffix stripping." Program 14.3 (1980): 130-137.
-
-only differing from it at the points maked --DEPARTURE-- and --NEW--
-below.
-
-For a more faithful version of the Porter algorithm, see
-
-    http://www.tartarus.org/~martin/PorterStemmer/
-
-Later additions:
-
-   June 2000
-   
-   The 'l' of the 'logi' -> 'log' rule is put with the stem, so that
-   short stems like 'geo' 'theo' etc work like 'archaeo' 'philo' etc.
-
-   This follows a suggestion of Barry Wilkins, reasearch student at
-   Birmingham.
-
-
-   February 2000
-
-   the cvc test for not dropping final -e now looks after vc at the
-   beginning of a word, so are, eve, ice, ore, use keep final -e. In this
-   test c is any consonant, including w, x and y. This extension was
-   suggested by Chris Emerson.
-
-   -fully    -> -ful   treated like  -fulness -> -ful, and
-   -tionally -> -tion  treated like  -tional  -> -tion
-
-   both in Step 2. These were suggested by Hiranmay Ghosh, of New Delhi.
-
-   Invariants proceed, succeed, exceed. Also suggested by Hiranmay Ghosh.
-"""
-
 import sys
 import re
 
 class PorterStemmer:
-
     def __init__(self):
-        """The main part of the stemming algorithm starts here.
-        b is a buffer holding a word to be stemmed. The letters are in b[k0],
-        b[k0+1] ... ending at b[k]. In fact k0 = 0 in this demo program. k is
-        readjusted downwards as the stemming progresses. Zero termination is
-        not in fact used in the algorithm.
-
-        Note that only lower case sequences are stemmed. Forcing to lower case
-        should be done before stem(...) is called.
-        """
-
         self.b = ""  # buffer for word to be stemmed 
         self.k = 0
         self.k0 = 0
         self.j = 0   # j is a general offset into the string
-
-        ## --NEW--
-        ## This is a table of irregular forms. It is quite short, but still
-        ## reflects the errors actually drawn to Martin Porter's attention over
-        ## a 20 year period!
-        ##
-        ## Extend it as necessary.
-        ##
-        ## The form of the table is:
-        ##  {
-        ##  "p1" : ["s11","s12","s13", ... ],
-        ##  "p2" : ["s21","s22","s23", ... ],
-        ##  ...
-        ##  "pn" : ["sn1","sn2","sn3", ... ]
-        ##  }
-        ##
-        ## String sij is mapped to paradigm form pi, and the main stemming
-        ## process is then bypassed.
-   
         irregular_forms = {
             "sky" :     ["sky", "skies"],
             "die" :     ["dying"],
@@ -119,12 +23,10 @@ class PorterStemmer:
             "exceed"  : ["exceed"],
             "succeed" : ["succeed"], # Hiranmay Ghosh
             }
-
         self.pool = {}
         for key in irregular_forms.keys():
             for val in irregular_forms[key]:
                 self.pool[val] = key
-
     def cons(self, i):
         """cons(i) is TRUE <=> b[i] is a consonant."""
         if self.b[i] == 'a' or self.b[i] == 'e' or self.b[i] == 'i' or self.b[i] == 'o' or self.b[i] == 'u':
@@ -135,18 +37,7 @@ class PorterStemmer:
             else:
                 return (not self.cons(i - 1))
         return 1
-
     def m(self):
-        """m() measures the number of consonant sequences between k0 and j.
-        if c is a consonant sequence and v a vowel sequence, and <..>
-        indicates arbitrary presence,
-        
-           <c><v>       gives 0
-           <c>vc<v>     gives 1
-           <c>vcvc<v>   gives 2
-           <c>vcvcvc<v> gives 3
-           ....
-        """
         n = 0
         i = self.k0
         while 1:
@@ -172,14 +63,12 @@ class PorterStemmer:
                     break
                 i = i + 1
             i = i + 1
-
     def vowelinstem(self):
         """vowelinstem() is TRUE <=> k0,...j contains a vowel"""
         for i in range(self.k0, self.j + 1):
             if not self.cons(i):
                 return 1
         return 0
-
     def doublec(self, j):
         """doublec(j) is TRUE <=> j,(j-1) contain a double consonant."""
         if j < (self.k0 + 1):
@@ -187,20 +76,7 @@ class PorterStemmer:
         if (self.b[j] != self.b[j-1]):
             return 0
         return self.cons(j)
-
     def cvc(self, i):
-        """cvc(i) is TRUE <=>
-
-        a) ( --NEW--) i == 1, and p[0] p[1] is vowel consonant, or
-
-        b) p[i - 2], p[i - 1], p[i] has the form consonant -
-           vowel - consonant and also if the second c is not w, x or y. this
-           is used when trying to restore an e at the end of a short word.
-           e.g.
-
-               cav(e), lov(e), hop(e), crim(e), but
-               snow, box, tray.        
-        """
         if i == 0: return 0  # i == 0 never happens perhaps
         if i == 1: return (not self.cons(0) and self.cons(1))
         if not self.cons(i) or self.cons(i-1) or not self.cons(i-2): return 0
@@ -208,11 +84,8 @@ class PorterStemmer:
         ch = self.b[i]
         if ch == 'w' or ch == 'x' or ch == 'y':
             return 0
-
         return 1        
-
     def ends(self, s):
-        """ends(s) is TRUE <=> k0,...k ends with the string s."""
         length = len(s)
         if s[length - 1] != self.b[self.k]: # tiny speed-up
             return 0
@@ -222,40 +95,14 @@ class PorterStemmer:
             return 0
         self.j = self.k - length
         return 1
-
     def setto(self, s):
-        """setto(s) sets (j+1),...k to the characters in the string s, readjusting k."""
         length = len(s)
         self.b = self.b[:self.j+1] + s + self.b[self.j+length+1:]
         self.k = self.j + length
-
     def r(self, s):
-        """r(s) is used further down."""
         if self.m() > 0:
             self.setto(s)
-
     def step1ab(self):
-        """step1ab() gets rid of plurals and -ed or -ing. e.g.
-        
-           caresses  ->  caress
-           ponies    ->  poni
-           sties     ->  sti
-           tie       ->  tie        (--NEW--: see below)
-           caress    ->  caress
-           cats      ->  cat
-        
-           feed      ->  feed
-           agreed    ->  agree
-           disabled  ->  disable
-        
-           matting   ->  mat
-           mating    ->  mate
-           meeting   ->  meet
-           milling   ->  mill
-           messing   ->  mess
-        
-           meetings  ->  meet
-        """
         if self.b[self.k] == 's':
             if self.ends("sses"):
                 self.k = self.k - 2
@@ -268,15 +115,11 @@ class PorterStemmer:
                     self.k = self.k - 2
             elif self.b[self.k - 1] != 's':
                 self.k = self.k - 1
-
         if self.ends("ied"):
             if self.j == 0:
                 self.k = self.k - 1
             else:
                 self.k = self.k - 2
-        # this line extends the original algorithm, so that
-        # 'spied'->'spi' but 'died'->'die' etc
-
         elif self.ends("eed"):
             if self.m() > 0:
                 self.k = self.k - 1
@@ -292,34 +135,10 @@ class PorterStemmer:
                     self.k = self.k + 1
             elif (self.m() == 1 and self.cvc(self.k)):
                 self.setto("e")
-
     def step1c(self):
-        """step1c() turns terminal y to i when there is another vowel in the stem.
-        --NEW--: This has been modified from the original Porter algorithm so that y->i
-        is only done when y is preceded by a consonant, but not if the stem
-        is only a single consonant, i.e.
-
-           (*c and not c) Y -> I
-
-        So 'happy' -> 'happi', but
-          'enjoy' -> 'enjoy'  etc
-
-        This is a much better rule. Formerly 'enjoy'->'enjoi' and 'enjoyment'->
-        'enjoy'. Step 1c is perhaps done too soon; but with this modification that
-        no longer really matters.
-
-        Also, the removal of the vowelinstem(z) condition means that 'spy', 'fly',
-        'try' ... stem to 'spi', 'fli', 'tri' and conflate with 'spied', 'tried',
-        'flies' ...
-        """
         if self.ends("y") and self.j > 0 and self.cons(self.k - 1):
             self.b = self.b[:self.k] + 'i' + self.b[self.k+1:]
-
     def step2(self):
-        """step2() maps double suffices to single ones.
-        so -ization ( = -ize plus -ation) maps to -ize etc. note that the
-        string before the suffix must give m() > 0.
-        """
         if self.b[self.k - 1] == 'a':
             if self.ends("ational"):   self.r("ate")
             elif self.ends("tional"):  self.r("tion")
@@ -357,8 +176,6 @@ class PorterStemmer:
             if self.ends("logi"):
                 self.j = self.j + 1     # --NEW-- (Barry Wilkins)
                 self.r("og")
-        # To match the published algorithm, delete this phrase
-
     def step3(self):
         """step3() dels with -ic-, -full, -ness etc. similar strategy to step2."""
         if self.b[self.k] == 'e':
@@ -372,7 +189,6 @@ class PorterStemmer:
             elif self.ends("ful"):     self.r("")
         elif self.b[self.k] == 's':
             if self.ends("ness"):      self.r("")
-
     def step4(self):
         """step4() takes off -ant, -ence etc., in context <c>vcvc<v>."""
         if self.b[self.k - 1] == 'a':
@@ -423,7 +239,6 @@ class PorterStemmer:
             return
         if self.m() > 1:
             self.k = self.j
-
     def step5(self):
         """step5() removes a final -e if m() > 1, and changes -ll to -l if
         m() > 1.
@@ -435,32 +250,14 @@ class PorterStemmer:
                 self.k = self.k - 1
         if self.b[self.k] == 'l' and self.doublec(self.k) and self.m() > 1:
             self.k = self.k -1
-
     def stem_word(self, p, i, j):
-        """In stem(p,i,j), p is a char pointer, and the string to be stemmed
-        is from p[i] to p[j] inclusive. Typically i is zero and j is the
-        offset to the last character of a string, (p[j+1] == '\0'). The
-        stemmer adjusts the characters p[i] ... p[j] and returns the new
-        end-point of the string, k. Stemming never increases word length, so
-        i <= k <= j. To turn the stemmer into a module, declare 'stem' as
-        extern, and delete the remainder of this file.
-        """
-        # copy the parameters into statics
         self.b = p
         self.k = j
         self.k0 = i
-
         if self.b[self.k0:self.k+1] in self.pool:
             return self.pool[self.b[self.k0:self.k+1]]
-        
         if self.k <= self.k0 + 1:
             return self.b # --DEPARTURE--
-
-        # With this line, strings of length 1 or 2 don't go through the
-        # stemming process, although no mention is made of this in the
-        # published algorithm. Remove the line to match the published
-        # algorithm.
-
         self.step1ab()
         self.step1c()
         self.step2()
@@ -468,36 +265,28 @@ class PorterStemmer:
         self.step4()
         self.step5()
         return self.b[self.k0:self.k+1]
-
     def adjust_case(self, word, stem):
         lower = word.lower()
-
         ret = ""
-
         for x in range(len(stem)):
             if lower[x] == stem[x]:
                 ret = ret + word[x]
             else:
                 ret = ret + stem[x]
-
         return ret
-
     def stem(self, text):
         parts = re.split(r"(\W+)", text)
         numWords = (len(parts) + 1)//2
-        
         ret = ""
         for i in range(numWords):
             word = parts[2 * i]
             separator = ""
             if ((2 * i) + 1) < len(parts):
                 separator = parts[(2 * i) + 1]
-
             stem = self.stem_word(word.lower(), 0, len(word) - 1)
             ret = ret + self.adjust_case(word, stem)
             ret = ret + separator
-        return ret
-            
+        return ret     
 if __name__ == '__main__':
     p = PorterStemmer()
     if len(sys.argv) > 1:
